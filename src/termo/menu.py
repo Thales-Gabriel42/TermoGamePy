f652b665b15d93c52c352db8ad5b4c2d6f357b49
Thales <thales.lima13@hotmail.com>
2024-06-14 15:30:56 -0300

    feat: add radioButtons to choose the game mode.

--- src/termo/menu.py
+++ src/termo/menu.py
@@ -8,8 +8,22 @@ class Menu(tk.Frame):
         self.parent = parent
         self._width = _width
         self._height = _height
-        tk.Button(self, text="Jogar", command=self.muda_tela).place(x=0, y=0)
+        self.place_widgets()
 
-    def muda_tela(self):
-        self.janela_jogo = Game(self.parent, self._width, self._height, self)
-        self.janela_jogo.place(x=0, y=0)
+    def place_widgets(self):
+        tk.Button(self, text="Play", command=self.screen_change).place(x=0, y=0)
+        modeOptions = tk.Frame(self, borderwidth=1, width=100, height=140, relief="solid")
+        modeOptions.place(x=50, y=30)
+        self.modeVar = tk.IntVar()
+        fourLetters = tk.Radiobutton(modeOptions, text="4 Letters", variable=self.modeVar, value=4)
+        fiveLetters = tk.Radiobutton(modeOptions, text="5 Letters", variable=self.modeVar, value=5)
+        sixLetters = tk.Radiobutton(modeOptions, text="6 Letters", variable=self.modeVar, value=6)
+        fourLetters.place(x=10, y=40, anchor='sw')
+        fiveLetters.place(x=10, y=80, anchor='sw')
+        sixLetters.place(x=10, y=120, anchor='sw')
+        fourLetters.select()
+
+    def screen_change(self):
+        self.game_window = Game(self.parent, self._width, self._height, self, self.modeVar.get())
+        self.game_window.place(x=0, y=0)
+
